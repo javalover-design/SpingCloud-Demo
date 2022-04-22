b2b17d8595ed93b3ee571c66e1a33f2d872ea818
javalover-design <2279818505@qq.com>
2022-04-22 17:47:42 +0800

    Eureka

---
+++ SpringCloud-eureka-7001/src/main/java/com/eureka/springcloud/EurekaServer.java
@@ -0,0 +1,16 @@
+package com.eureka.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author lambda
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class,args);
+    }
+}
